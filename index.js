fea8ca2a2cbfa38a22bbf245a8a2f65dfb1b9f48
gabrielrosin <gabriel.s.rosin@gmail.com>
2022-08-26 08:53:10 -0300

    Includes CORS policy in the API and finishes the project without bonus

--- index.js
+++ index.js
@@ -1,69 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 
-const users = [
-    {
-        username: "spongebob",
-        avatar: "spongebob"
-    },
-    {
-        username: "jonas",
-        avatar: "jonas"
-    },
-];
-const tweets = [
-    {
-        username: "spongebob",
-        tweet: "I love krabby patties 1"
-    },
-    {
-        username: "spongebob",
-        tweet: "I love krabby patties 2"
-    },
-    {
-        username: "spongebob",
-        tweet: "I love krabby patties 3"
-    },
-    {
-        username: "spongebob",
-        tweet: "I love krabby patties 4"
-    },
-    {
-        username: "spongebob",
-        tweet: "I love krabby patties 5"
-    },
-    {
-        username: "spongebob",
-        tweet: "I love krabby patties 6"
-    },
-    {
-        username: "spongebob",
-        tweet: "I love krabby patties 7"
-    },
-    {
-        username: "jonas",
-        tweet: "I love krabby patties 8"
-    },
-    {
-        username: "spongebob",
-        tweet: "I love krabby patties 9"
-    },
-    {
-        username: "spongebob",
-        tweet: "I love krabby patties 10"
-    },
-    {
-        username: "spongebob",
-        tweet: "I love krabby patties 11"
-    },
-    {
-        username: "spongebob",
-        tweet: "I love krabby patties 12"
-    },
-];
+const users = [];
+const tweets = [];
 
 server.get("/tweets", (req, res) => {
     const lastTweets = tweets.slice(-10);
