03f3e2c49d56bf13d212c229a02db9824686321f
gabrielrosin <gabriel.s.rosin@gmail.com>
2022-08-28 18:23:52 -0300

    BONUS: Implements bonus, that consists on requests for loading a limited amount of tweets through query string, enables checking all the tweets from one user, includes some POST infos being passed through headers and includes some code status for errors and success

--- index.js
+++ index.js
@@ -9,30 +9,73 @@ server.use(express.json());
 const users = [];
 const tweets = [];
 
-server.get("/tweets", (req, res) => {
-    const lastTweets = tweets.slice(-10);
-    lastTweets.forEach(tweet => {
+function getAvatar(tweets) {
+    tweets.forEach(tweet => {
         const userObj = users.find(user => user.username === tweet.username);
         tweet.avatar = userObj.avatar;
-    });
-    res.send(lastTweets);
+    })
+
+    return tweets
+}
+
+server.get("/tweets", (req, res) => {
+    const reverseTweets = tweets.slice().reverse();
+    const page = Number(req.query.page);
+
+    if (page < 1) {
+        return res.status(400).send("Informe uma página válida!");
+    }
+
+    const lastTweets = reverseTweets.slice((page-1)*10, page*10);
+    console.log(getAvatar(reverseTweets));
+
+    res.send((lastTweets));
+});
+
+server.get("/tweets/:username", (req, res) => {
+    const {username} = req.params;
+
+    const userTweets = tweets.filter(tweet => tweet.username === username);
+
+    res.send(getAvatar(userTweets));
 });
 
 server.post("/tweets", (req, res) => {
-    tweets.push(req.body);
+    const {tweet} = req.body;
+    const {user} = req.headers;
 
-    res.send("OK");
+    if (!user || !tweet) {
+        res.status(400).send({error: "Todos os campos são obrigatórios!"});
+        return;
+    }
+
+    tweets.push(
+        {
+            username: user,
+            tweet,
+        }
+    );
+
+    res.status(201).send({message: "OK"});
 });
 
 server.post("/sign-up", (req, res) => {
-    users.push(req.body);
+    const {username, avatar} = req.body;
 
-    res.send("OK");
+    if (!username || !avatar) {
+        res.status(400).send({error: "Todos os campos são obrigatórios!"});
+        return;
+    }
+
+    users.push(
+        {
+            username,
+            avatar,
+        }
+    );
+
+    res.status(201).send({message: "OK"});
 });
-
-// server.get("/users", (req, res) => {
-//     res.send(users);
-// });
 
 
 server.listen(5000, () => console.log("Listening to port 5000"));
